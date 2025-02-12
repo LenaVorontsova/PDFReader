a601d4a61541024b3d455bff73f95b354fce592d
elenavorontsova <lena.voroncova26@yandex.ru>
2025-02-12 14:05:15 +0400

    create the project

---
+++ PDFReader/PDFReader/PDFReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFReaderApp.swift
+//  PDFReader
+//
+//  Created by Елена Воронцова on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PDFReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
